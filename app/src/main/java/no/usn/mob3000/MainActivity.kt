43d5d640e783e28c4d48b09d57c10679e9cf3137
frigvid <67565373+frigvid@users.noreply.github.com>
2024-09-11 16:08:44 +0200

    feat: Add drawables, add localizations.

--- app/src/main/java/no/usn/mob3000/MainActivity.kt
+++ app/src/main/java/no/usn/mob3000/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.navigation.ui.setupWithNavController
 import no.usn.mob3000.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +21,16 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        /* Passing each menu ID as a set of Ids because each menu should be considered as top
+         * level destinations.
+         */
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
