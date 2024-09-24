b571e93aba92e764367fe0b9ebbbb6490cc6cf04
frigvid <67565373+frigvid@users.noreply.github.com>
2024-09-24 15:59:58 +0200

    feat: Rework navigation. Should be less annoying now.

---
+++ app/src/main/java/no/usn/mob3000/ui/screens/train/OpeningsScreen.kt
@@ -0,0 +1,17 @@
+package no.usn.mob3000.ui.screens.train
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import no.usn.mob3000.R
+
+@Composable
+fun OpeningsScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("TEMP_STRING: Openings")
+    }
+}
