49d297ad52d25dd99585bab5a2a6adaf2e7a20c4
frigvid <67565373+frigvid@users.noreply.github.com>
2024-10-02 08:53:25 +0200

    feat: Rework navigation some more. Allow screens to override or add to the topBar or bottomBar.

--- app/src/main/java/no/usn/mob3000/App.kt
+++ app/src/main/java/no/usn/mob3000/App.kt
@@ -2,8 +2,9 @@ package no.usn.mob3000
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -20,6 +21,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,6 +30,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -45,11 +49,15 @@ import no.usn.mob3000.ui.screens.auth.LoginScreen
 import no.usn.mob3000.ui.screens.auth.ResetPasswordScreen
 import no.usn.mob3000.ui.screens.chess.HistoryScreen
 import no.usn.mob3000.ui.screens.chess.PlayScreen
+import no.usn.mob3000.ui.screens.chess.train.CreateGroupScreen
+import no.usn.mob3000.ui.screens.chess.train.CreateOpeningScreen
 import no.usn.mob3000.ui.screens.info.FAQScreen
 import no.usn.mob3000.ui.screens.chess.train.GroupsScreen
 import no.usn.mob3000.ui.screens.chess.train.OpeningsScreen
 import no.usn.mob3000.ui.theme.NavbarBackground
 import no.usn.mob3000.ui.theme.NavbarButtonSelected
+
+val LocalNavController = compositionLocalOf<NavHostController> { error("No NavController found!") }
 
 /**
  * This file is the main navigation point for the application, and serves as a place
@@ -70,77 +78,11 @@ import no.usn.mob3000.ui.theme.NavbarButtonSelected
 fun App(
     navController: NavHostController = rememberNavController()
 ) {
-    val backStackEntry by navController.currentBackStackEntryAsState()
-    val currentDestination = backStackEntry?.destination
-    val currentScreen = Screen.valueOf(
-        backStackEntry?.destination?.route ?: Screen.HOME.name
-    )
-
-    /* Since the navigation bar should only have some of the routes added,
-     * this is the somewhat dirty work-around. We could just make a second
-     * route enum class for base routes, and honestly, we probably should,
-     * since regular routes have no need for an icon.
-     */
-    val rootEntries = listOf(
-        Screen.DOCUMENTATION,
-        Screen.NEWS,
-        Screen.HOME,
-        Screen.PROFILE,
-        Screen.SETTINGS
-    )
-
-    Scaffold(
-        topBar = {
-            TopNavbar(
-                currentScreen = currentScreen,
-                canNavigateBack = navController.previousBackStackEntry != null,
-                navigateUp = { navController.navigateUp() },
-                roots = rootEntries
-            )
-        },
-        bottomBar = {
-            /* TODO: Extract this. */
-            BottomAppBar(
-                containerColor = NavbarBackground
-            ) {
-                rootEntries.forEach { screen ->
-                    NavigationBarItem(
-                        icon = {
-                            screen.icon?.let {
-                                ScreenIcon(
-                                    it,
-                                    modifier = Modifier.size(24.dp)
-                                )
-                            }
-                        },
-                        label = null, // Alternatively: label = { Text(stringResource(screen.title)) },
-                        selected = currentDestination?.hierarchy?.any { it.route == screen.name } == true,
-                        onClick = {
-                            navController.navigate(screen.name) {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = true
-                                }
-
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        },
-                        colors = NavigationBarItemDefaults.colors(
-                            selectedIconColor = Color.White,
-                            indicatorColor = NavbarButtonSelected
-                        )
-                    )
-                }
-            }
-        }
-    ) { innerPadding ->
+    CompositionLocalProvider(LocalNavController provides navController) {
         NavHost(
             navController = navController,
             startDestination = Screen.HOME.name,
-            modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState())
-                .padding(innerPadding)
+            modifier = Modifier.fillMaxSize()
         ) {
             composable(route = Screen.DOCUMENTATION.name) { DocumentationScreen() }
             composable(route = Screen.FAQ.name) { FAQScreen() }
@@ -167,6 +109,94 @@ fun App(
 }
 
 /**
+ * This function serves as the default viewport that screens need to implement.
+ *
+ * @author frigvid
+ * @created 2024-10-01
+ */
+@Composable
+fun Viewport(
+    topBar: @Composable (
+        currentScreen: Screen,
+        canNavigateBack: Boolean,
+        navigateUp: () -> Unit,
+        modifier: Modifier,
+        roots: List<Screen>,
+        showTitle: Boolean,
+        topBarActions: @Composable (RowScope.() -> Unit)
+    ) -> Unit = { currentScreen, canNavigateBack, navigateUp, modifier, roots, showTitle, topBarActions ->
+        TopNavbar(
+            currentScreen = currentScreen,
+            canNavigateBack = canNavigateBack,
+            navigateUp = navigateUp,
+            modifier = modifier,
+            roots = roots,
+            showTitle = showTitle,
+            topBarActions = topBarActions
+        )
+    },
+    bottomBar: @Composable (
+        rootEntries: List<Screen>,
+        currentDestination: NavDestination?,
+        onNavigate: (String) -> Unit
+    ) -> Unit = { rootEntries, currentDestination, onNavigate ->
+        BottomNavbar(
+            rootEntries = rootEntries,
+            currentDestination = currentDestination,
+            onNavigate = onNavigate
+        )
+    },
+    hideTitle: Boolean = true,
+    floatingActionButton: @Composable () -> Unit = {},
+    topBarActions: @Composable (RowScope.() -> Unit) = {},
+    content: @Composable (PaddingValues) -> Unit
+) {
+    val navController = LocalNavController.current
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = currentBackStackEntry?.destination
+    val currentScreen = Screen.valueOf(
+        currentBackStackEntry?.destination?.route ?: Screen.HOME.name
+    )
+
+    val rootEntries = listOf(
+        Screen.DOCUMENTATION,
+        Screen.NEWS,
+        Screen.HOME,
+        Screen.PROFILE,
+        Screen.SETTINGS
+    )
+
+    Scaffold(
+        topBar = {
+            topBar(
+                currentScreen,
+                navController.previousBackStackEntry != null,
+                { navController.navigateUp() },
+                Modifier,
+                rootEntries,
+                hideTitle,
+                topBarActions
+            )
+        },
+        bottomBar = {
+            bottomBar(
+                rootEntries,
+                currentDestination
+            ) { route ->
+                navController.navigate(route) {
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+        },
+        floatingActionButton = floatingActionButton
+    ) { innerPadding -> content(innerPadding) }
+}
+
+/**
  * This enum class defines whether a route exists or not.
  *
  * @param title The title, usually shown in the TopAppBar.
@@ -174,30 +204,33 @@ fun App(
  * @author frigvid
  * @created 2024-09-24
  */
-enum class Screen(@StringRes val title: Int, val icon: Icon?) {
+enum class Screen(@StringRes val title: Int, val icon: Icon? = null) {
     DOCUMENTATION(title = R.string.docs_title, icon = Icon.DrawableResourceIcon(R.drawable.navbar_documentation)),
-    FAQ(title = R.string.faq_title, icon = null),
+    FAQ(title = R.string.faq_title),
     NEWS(title = R.string.news_title, icon = Icon.DrawableResourceIcon(R.drawable.navbar_news)),
     HOME(title = R.string.home_title, icon = Icon.DrawableResourceIcon(R.drawable.navbar_home)),
     PROFILE(title = R.string.profile_title, icon = Icon.DrawableResourceIcon(R.drawable.navbar_profile)),
     SETTINGS(title = R.string.settings_title, icon = Icon.DrawableResourceIcon(R.drawable.navbar_settings)),
-    OPENINGS(title = R.string.openings_title, icon = null),
-    GROUPS(title = R.string.groups_title, icon = null),
-    PLAY(title = R.string.home_play_title, icon = null),
-    HISTORY(title = R.string.home_history_title, icon = null),
-    AUTH_LOGIN(title = R.string.auth_login_title, icon = null),
-    AUTH_CREATE(title = R.string.auth_createUser_title, icon = null),
-    AUTH_FORGOT(title = R.string.auth_forgotPassword_title, icon = null),
-    AUTH_RESET(title = R.string.auth_resetPassword_title, icon = null)
+    OPENINGS(title = R.string.openings_title),
+    OPENINGS_CREATE(title = R.string.openings_create_title),
+    GROUPS(title = R.string.groups_title),
+    GROUPS_CREATE(title = R.string.groups_create_title),
+    PLAY(title = R.string.home_play_title),
+    HISTORY(title = R.string.home_history_title),
+    AUTH_LOGIN(title = R.string.auth_login_title),
+    AUTH_CREATE(title = R.string.auth_createUser_title),
+    AUTH_FORGOT(title = R.string.auth_forgotPassword_title),
+    AUTH_RESET(title = R.string.auth_resetPassword_title)
 }
 
 /**
- * This is the navigation bar at the top of the viewport. Primarily, it serves
- * the purpose of displaying the title of the page, and providing a navigation
- * point back to the root page.
+ * This is the navigation bar wrapper at the top of the viewport. Primarily, it serves
+ * the purpose of displaying the title of the page, and providing a navigation point
+ * back to the root page.
  *
  * TODO: Show button title/label when button is pressed-and-held for a length of time.
  *       Once released, it should not fire a click-event. That'd suck.
+ *
  * @param currentScreen The current route.
  * @param canNavigateBack This decides if you have a way to navigate back to the root.
  *                        Note that if a Screen enums is given through <roots>, this
@@ -215,10 +248,12 @@ fun TopNavbar(
     canNavigateBack: Boolean,
     navigateUp: () -> Unit,
     modifier: Modifier = Modifier,
-    roots: List<Screen>
+    roots: List<Screen>,
+    showTitle: Boolean = true,
+    topBarActions: @Composable (RowScope.() -> Unit) = {},
 ) {
     TopAppBar(
-        title = { Text(stringResource(currentScreen.title)) },
+        title = { if (showTitle) Text(stringResource(currentScreen.title)) },
         colors = TopAppBarDefaults.mediumTopAppBarColors(
             containerColor = NavbarBackground
         ),
@@ -236,8 +271,40 @@ fun TopNavbar(
                     )
                 }
             }
-        }
+        },
+        actions = topBarActions
     )
+}
+
+@Composable
+fun BottomNavbar(
+    rootEntries: List<Screen>,
+    currentDestination: NavDestination?,
+    onNavigate: (String) -> Unit
+) {
+    BottomAppBar(
+        containerColor = NavbarBackground
+    ) {
+        rootEntries.forEach { screen ->
+            NavigationBarItem(
+                icon = {
+                    screen.icon?.let {
+                        ScreenIcon(
+                            it,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                },
+                label = null,
+                selected = currentDestination?.hierarchy?.any { it.route == screen.name } == true,
+                onClick = { onNavigate(screen.name) },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Color.White,
+                    indicatorColor = NavbarButtonSelected
+                )
+            )
+        }
+    }
 }
 
 /**
