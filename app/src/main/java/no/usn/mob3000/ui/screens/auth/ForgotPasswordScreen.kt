676609ca64168b00797751aa2542c9de80368b56
Anarox1111 <68535747+Anarox1111@users.noreply.github.com>
2024-10-06 19:38:42 +0200

    ForgotPasswordScreen.kt has now a UI.

--- app/src/main/java/no/usn/mob3000/ui/screens/auth/ForgotPasswordScreen.kt
+++ app/src/main/java/no/usn/mob3000/ui/screens/auth/ForgotPasswordScreen.kt
@@ -5,11 +5,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import no.usn.mob3000.R
 import no.usn.mob3000.Viewport
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 /**
  * This is the chessboard page, where users can free-play against an AI or
@@ -24,11 +35,42 @@ import no.usn.mob3000.Viewport
 fun ForgotPasswordScreen() {
     Viewport { innerPadding ->
         Box(
-            modifier = Modifier.fillMaxSize()
-                               .padding(innerPadding),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
             contentAlignment = Alignment.Center
         ) {
-            Text(stringResource(R.string.auth_forgotPassword_title))
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp) // Legg til litt padding
+            ) {
+
+                Text(
+                    text = "Type in your email that is linked to your account. You will recieve a mail with a link to reset your password.",
+                    fontSize = 12.sp
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                // TextField with placeholder text
+                var email by remember { mutableStateOf("") }
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    placeholder = { Text("Your email") },
+                    label = { Text("Email") },
+                    modifier = Modifier.fillMaxWidth(0.8f)
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Button(
+                    onClick = {},
+//                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Reset password")
+                }
+            }
         }
     }
 }
