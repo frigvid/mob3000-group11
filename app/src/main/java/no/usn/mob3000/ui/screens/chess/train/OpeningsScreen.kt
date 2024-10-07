241d0ffceaa200eaf4467a03d3d0f54876afdff7
frigvid <67565373+frigvid@users.noreply.github.com>
2024-10-07 18:57:16 +0200

    chore: Revert example in preperation for pull request.

--- app/src/main/java/no/usn/mob3000/ui/screens/chess/train/OpeningsScreen.kt
+++ app/src/main/java/no/usn/mob3000/ui/screens/chess/train/OpeningsScreen.kt
@@ -1,6 +1,5 @@
 package no.usn.mob3000.ui.screens.chess.train
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,16 +8,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import io.github.jan.supabase.postgrest.from
-import io.github.jan.supabase.postgrest.postgrest
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
 import no.usn.mob3000.Viewport
-import no.usn.mob3000.data.SupabaseClientWrapper
-import java.util.UUID
 
 /**
  * This shows the various chess openings that are available by default, and that
@@ -39,22 +29,6 @@ fun OpeningsScreen(
     onCreateOpeningClick: () -> Unit,
     filter: List<String>? = null
 ) {
-    /* Temporary example on how to fetch data from the back-end. */
-    LaunchedEffect(key1 = true) {
-        try {
-            val result = withContext(Dispatchers.IO) {
-                val supabase = SupabaseClientWrapper.getClient()
-                supabase.from("public", "openings").select {
-                    filter {
-                        eq("id", "0976138f-46fe-45b5-aa7e-9272f4dbab87")
-                    }
-                }.decodeSingle<Opening>()
-            }
-            Log.d("OpeningsScreen", "Fetched opening data: $result")
-        } catch (e: Exception) {
-            Log.e("OpeningsScreen", "Error fetching opening", e)
-        }
-    }
 
     Viewport (
         floatingActionButton = {
@@ -75,14 +49,3 @@ fun OpeningsScreen(
         }
     }
 }
-
-/* Temporary serializable data class. */
-@Serializable
-data class Opening(
-    val id: String,
-    val created_by: String? = null,
-    val title: String,
-    val description: String,
-    val pgn: List<Map<String, String>>,
-    val timestamp: String
-)
