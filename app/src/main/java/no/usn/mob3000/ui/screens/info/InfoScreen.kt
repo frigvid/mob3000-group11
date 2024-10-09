2722b669e84f42a1f0419cbb9fb8c7f565c838ed
frigvid <67565373+frigvid@users.noreply.github.com>
2024-10-09 19:48:50 +0200

    feat: Implement navigation buttons to About us, documentation and FAQ screens from Information screen.

--- app/src/main/java/no/usn/mob3000/ui/screens/info/InfoScreen.kt
+++ app/src/main/java/no/usn/mob3000/ui/screens/info/InfoScreen.kt
@@ -1,24 +1,29 @@
 package no.usn.mob3000.ui.screens.info
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import no.usn.mob3000.R
 import no.usn.mob3000.Viewport
+import no.usn.mob3000.ui.theme.DefaultButton
 
 /**
  * This screen serves as an entry point for the various other documentation screens.
  *
+ * @param onAboutUsClick Callback function to navigate to [AboutUsScreen].
+ * @param onDocumentationClick Callback function to navigate to [DocumentationScreen].
+ * @param onFAQClick Callback function to navigate to [FAQScreen].
  * @see [DocumentationScreen]
  * @see [FAQScreen]
  * @see [AboutUsScreen]
  * @author frigvid
- * @created 2024-09-12
+ * @created 2024-10-09
  */
 @Composable
 fun InfoScreen(
@@ -27,12 +32,54 @@ fun InfoScreen(
     onFAQClick: () -> Unit,
 ) {
     Viewport { innerPadding ->
-        Box(
-            modifier = Modifier.fillMaxSize()
-                .padding(innerPadding),
-            contentAlignment = Alignment.Center
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            /* TODO: 3 buttons that go to the different documentation pages. */
+            InfoButton(
+                text = stringResource(R.string.about_us_title),
+                onClick = onAboutUsClick
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            InfoButton(
+                text = stringResource(R.string.docs_title),
+                onClick = onDocumentationClick
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            InfoButton(
+                text = stringResource(R.string.faq_title),
+                onClick = onFAQClick
+            )
         }
     }
+}
+
+/**
+ * Re-usable "large" button component.
+ *
+ * @param text The text to display on the button.
+ * @param onClick The action(s) to execute when clicked.
+ * @author frigvid
+ * @created 2024-10-09
+ */
+@Composable
+private fun InfoButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(containerColor = DefaultButton),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+    ) { Text(text) }
 }
