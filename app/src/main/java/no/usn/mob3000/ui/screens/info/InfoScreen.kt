90886a2f0ecf2648e138a75a3376c6e0a7195f00
frigvid <67565373+frigvid@users.noreply.github.com>
2024-10-09 19:31:18 +0200

    feat: Add AbousUs screen and InfoScreen. No routes yet.

---
+++ app/src/main/java/no/usn/mob3000/ui/screens/info/InfoScreen.kt
@@ -0,0 +1,34 @@
+package no.usn.mob3000.ui.screens.info
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import no.usn.mob3000.R
+import no.usn.mob3000.Viewport
+
+/**
+ * This screen serves as an entry point for the various other documentation screens.
+ *
+ * @see [DocumentationScreen]
+ * @see [FAQScreen]
+ * @see [AboutUsScreen]
+ * @author frigvid
+ * @created 2024-09-12
+ */
+@Composable
+fun InfoScreen() {
+    Viewport { innerPadding ->
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            /* TODO: 3 buttons that go to the different documentation pages. */
+        }
+    }
+}
