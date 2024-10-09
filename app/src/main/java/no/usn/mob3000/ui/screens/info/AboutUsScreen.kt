56b2e70c45a4a1ea9926fb6ed99d75b1f2647c2a
frigvid <67565373+frigvid@users.noreply.github.com>
2024-10-09 20:10:00 +0200

    feat: Implementation of about us UI.

--- app/src/main/java/no/usn/mob3000/ui/screens/info/AboutUsScreen.kt
+++ app/src/main/java/no/usn/mob3000/ui/screens/info/AboutUsScreen.kt
@@ -1,13 +1,19 @@
 package no.usn.mob3000.ui.screens.info
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import no.usn.mob3000.R
 import no.usn.mob3000.Viewport
 
@@ -20,12 +26,82 @@ import no.usn.mob3000.Viewport
 @Composable
 fun AboutUsScreen() {
     Viewport { innerPadding ->
-        Box(
-            modifier = Modifier.fillMaxSize()
-                .padding(innerPadding),
-            contentAlignment = Alignment.Center
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState())
         ) {
-            Text(stringResource(R.string.about_us_title))
+            Text(
+                text = stringResource(R.string.about_us_preamble1),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.about_us_preamble2),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.about_us_preamble3),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Text(
+                text = "How Chess Buddy helps you",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
+                fontStyle = FontStyle.Italic,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Text("1. " + stringResource(R.string.about_us_how_we_help1))
+                Text("2. " + stringResource(R.string.about_us_how_we_help2))
+                Text("3. " + stringResource(R.string.about_us_how_we_help3))
+                Text("4. " + stringResource(R.string.about_us_how_we_help4))
+            }
+
+            Text(
+                text = "About us",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.about_us_the_company1),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.about_us_the_company2),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.about_us_the_company3),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.about_us_vision_title),
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
+                fontStyle = FontStyle.Italic,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.about_us_vision),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
         }
     }
 }
