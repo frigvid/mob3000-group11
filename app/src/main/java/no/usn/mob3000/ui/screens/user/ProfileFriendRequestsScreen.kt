5106550fcc413e96f1dc75ccb58a062828d08aec
frigvid <67565373+frigvid@users.noreply.github.com>
2024-10-12 14:30:22 +0200

    feat: Implement initial profile friend request UI.
    Since there ain't no data layer operations yet, this is *really* simple. Must obviously be expanded upon later.

--- app/src/main/java/no/usn/mob3000/ui/screens/user/ProfileFriendRequestsScreen.kt
+++ app/src/main/java/no/usn/mob3000/ui/screens/user/ProfileFriendRequestsScreen.kt
@@ -7,21 +7,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import no.usn.mob3000.R
 import no.usn.mob3000.Viewport
 
 /**
  * Screen to allow users to see pending friend requests for their acccounts,
  * and accept or reject them.
  *
- * @author Hussein
+ * TODO: Add data layer functionality that actually fetches friend requests in the background.
+ * TODO: Cache known, but unhandled, friend requests for the user in ViewModel state.
+ *
+ * @author Hussein, frigvid
  * @created 2024-10-11
  */
 @Composable
 fun ProfileFriendRequestsScreen() {
     Viewport { innerPadding ->
         Box(
-            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
             contentAlignment = Alignment.Center
-        ) { Text("Pending friend requests.") }
+        ) {
+            /* TODO: If-else block to check if cached list is empty, wait for it to update first.
+             *       Show buffering if waiting. If background fetching, that'll hopefully not be
+             *       necessary though.
+             */
+            Text(stringResource(R.string.profile_pending_friend_requests_no_requests))
+        }
     }
 }
