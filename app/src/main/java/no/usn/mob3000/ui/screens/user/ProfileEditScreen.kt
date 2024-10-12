22d3469f7e9420b435d4df9377715e5de2e4324f
frigvid <67565373+frigvid@users.noreply.github.com>
2024-10-12 14:43:48 +0200

    chore: Remove unused imports.

--- app/src/main/java/no/usn/mob3000/ui/screens/user/ProfileEditScreen.kt
+++ app/src/main/java/no/usn/mob3000/ui/screens/user/ProfileEditScreen.kt
@@ -9,9 +9,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import no.usn.mob3000.R
 import no.usn.mob3000.Viewport
 
