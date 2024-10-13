2980f5ad21bf8cbe7c63ae02cfd6baec8cc25172
frigvid <67565373+frigvid@users.noreply.github.com>
2024-10-13 13:16:19 +0200

    refactor: Move button to topBarAction.

--- app/src/main/java/no/usn/mob3000/ui/screens/ProfileScreen.kt
+++ app/src/main/java/no/usn/mob3000/ui/screens/ProfileScreen.kt
@@ -7,11 +7,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import no.usn.mob3000.R
@@ -25,7 +32,17 @@ import no.usn.mob3000.Viewport
 fun ProfileScreen(
     onLogin: () -> Unit
 ) {
-    Viewport { innerPadding ->
+    Viewport (
+        topBarActions = {
+            IconButton(
+                /* TODO: Replace with DefaultButton theme color when up-to-date with master. */
+                colors = IconButtonDefaults.iconButtonColors(Color(0XFFC0A080)),
+                onClick = onLogin
+            ) {
+                Icon(Icons.Default.PlayArrow, contentDescription = "Groups")
+            }
+        }
+    ) { innerPadding ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -35,18 +52,7 @@ fun ProfileScreen(
             Column(
                 modifier = Modifier.padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(stringResource(R.string.profile_title))
-
-                Spacer(modifier = Modifier.height(10.dp))
-
-                Button(
-                    onClick = { onLogin() },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("midlertidig Log In path")
-                }
-            }
+            ) { Text(stringResource(R.string.profile_title)) }
         }
     }
 }
