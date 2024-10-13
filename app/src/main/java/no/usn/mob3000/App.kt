c2bd35937da4ce93b5c47443357f0dccdd7ba4e2
frigvid <67565373+frigvid@users.noreply.github.com>
2024-10-13 13:28:07 +0200

    fix: Sloppy merge leftovers.

--- app/src/main/java/no/usn/mob3000/App.kt
+++ app/src/main/java/no/usn/mob3000/App.kt
@@ -483,9 +483,9 @@ enum class Destination(@StringRes val title: Int, val icon: Icon? = null) {
     PLAY(title = R.string.home_play_title),
     HISTORY(title = R.string.home_history_title),
     AUTH_LOGIN(title = R.string.auth_login_title),
-    AUTH_CREATE(title = R.string.auth_createUser_title),
-    AUTH_FORGOT(title = R.string.auth_forgotPassword_title),
-    AUTH_RESET(title = R.string.auth_resetPassword_title),
+    AUTH_CREATE(title = R.string.auth_create_user_title),
+    AUTH_FORGOT(title = R.string.auth_forgot_password_title),
+    AUTH_RESET(title = R.string.auth_reset_password_title),
     ADMIN_DASHBOARD(title = R.string.admin_dashboard_title)
 }
 
