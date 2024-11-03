4e8c2ab2df053c5aad4cd8693d36e58548c61a95
frigvid <67565373+frigvid@users.noreply.github.com>
2024-11-04 00:54:25 +0100

    fix: Ensure that status information for info items are always aligned towards the bottom.

--- app/src/main/java/no/usn/mob3000/ui/components/info/ContentDisplay.kt
+++ app/src/main/java/no/usn/mob3000/ui/components/info/ContentDisplay.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -15,6 +16,7 @@ import androidx.compose.ui.unit.sp
  * Content display for news, documentation and faq.
  *
  * @author 258030
+ * @contributor frigvid
  * @created 2024-10-30
  */
 @Composable
@@ -29,12 +31,12 @@ fun ContentDisplay(
     Box(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
-                .verticalScroll(rememberScrollState())
         ) {
             Text(
                 text = title,
@@ -60,7 +62,14 @@ fun ContentDisplay(
                     modifier = Modifier.padding(bottom = 16.dp)
                 )
             }
+        }
 
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
             createdAt?.let {
                 Text(
                     text = formatInstant(it, isCreated = true),
