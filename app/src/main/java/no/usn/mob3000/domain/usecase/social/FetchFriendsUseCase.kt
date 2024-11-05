6d392a3f62f6bed8be928fe073bbbe8f4ee890b5
mr.Hussein Abdul-Ameer <husseinabdulameer11@gmail.com>
2024-11-05 18:05:00 +0100

    Update FetchFriendsUseCase.kt

--- app/src/main/java/no/usn/mob3000/domain/usecase/social/FetchFriendsUseCase.kt
+++ app/src/main/java/no/usn/mob3000/domain/usecase/social/FetchFriendsUseCase.kt
@@ -1,9 +1,5 @@
 package no.usn.mob3000.domain.usecase.social
 
-/**
- * @author Hussein Abdul-Ameer
- * created on 03.11.2024
- */
 import no.usn.mob3000.data.repository.social.FriendsRepository
 import no.usn.mob3000.data.source.remote.auth.AuthDataSource
 import no.usn.mob3000.domain.model.social.FriendData
