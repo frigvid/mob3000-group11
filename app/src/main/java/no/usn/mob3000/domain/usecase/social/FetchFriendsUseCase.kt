44ae9b03a16130b40c849913576bd3fcb0223fa6
mr.Hussein Abdul-Ameer <husseinabdulameer11@gmail.com>
2024-11-05 18:03:34 +0100

    Update FetchFriendsUseCase.kt

--- app/src/main/java/no/usn/mob3000/domain/usecase/social/FetchFriendsUseCase.kt
+++ app/src/main/java/no/usn/mob3000/domain/usecase/social/FetchFriendsUseCase.kt
@@ -5,11 +5,13 @@ package no.usn.mob3000.domain.usecase.social
  * created on 03.11.2024
  */
 import no.usn.mob3000.data.repository.social.FriendsRepository
-import no.usn.mob3000.domain.model.DocsData
-import no.usn.mob3000.domain.model.Friend
+import no.usn.mob3000.data.source.remote.auth.AuthDataSource
+import no.usn.mob3000.domain.model.social.FriendData
 
-class FetchFriendsUseCase(private val fetchRepository: FriendsRepository = FriendsRepository()) {
-    suspend fun fetchFriends(): Result<List<Friend>> {
-        return fetchRepository.fetchFriends()
+class FetchFriendsUseCase (
+    private val fetchrepository: FriendsRepository = FriendsRepository()
+){
+    suspend fun fetchFriends():Result<List<FriendData>> {
+        return fetchrepository.FetchFriends()
     }
 }
