cb6776049fd87463170c4ae5d6e8388de608274c
husseinabdulameer11 <husseinabdulameer11@gmail.com>
2024-11-15 23:30:30 +0100

    added logic to send the user to the settings page(used for loging in) when the user try to access the profile page while not being authenticated

--- app/src/main/java/no/usn/mob3000/ui/screens/user/ProfileScreen.kt
+++ app/src/main/java/no/usn/mob3000/ui/screens/user/ProfileScreen.kt
@@ -1,5 +1,6 @@
 package no.usn.mob3000.ui.screens.user
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.material3.Text
@@ -21,6 +22,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon;
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.LaunchedEffect
@@ -30,9 +33,12 @@ import androidx.compose.ui.res.stringResource
 import kotlinx.coroutines.flow.StateFlow
 import no.usn.mob3000.ui.components.base.Viewport
 import no.usn.mob3000.domain.model.auth.UserProfile
+import no.usn.mob3000.domain.model.auth.state.AuthenticationState
 import no.usn.mob3000.domain.model.social.FriendData
 import no.usn.mob3000.ui.theme.ProfileUserBackground
 import no.usn.mob3000.ui.theme.ProfileUserStatisticsBackground
+import androidx.compose.runtime.*;
+import no.usn.mob3000.ui.theme.DefaultButton
 
 /**
  * The profile screen.
@@ -58,13 +64,17 @@ fun ProfileScreen(
     friendState: StateFlow<Result<List<FriendData>>>,
     userIdState: StateFlow<String?>,
     userProfilesMap: StateFlow<Map<String, UserProfile>>,
-    setSelectedUser: (UserProfile) -> Unit
+    setSelectedUser: (UserProfile) -> Unit,
+    authenticationState: StateFlow<AuthenticationState>,
+    authenticationStateUpdate: () -> Unit,
+    onLoginClick: () -> Unit
 ) {
     val friendResult by friendState.collectAsState()
     val userId by userIdState.collectAsState()
     val userProfiles by userProfilesMap.collectAsState()
-
+    val state by remember {authenticationState}.collectAsState()
     LaunchedEffect(userId) {
+        authenticationStateUpdate()
         userId?.let { id ->
             fetchUser(id)
             fetchFriends()
@@ -109,15 +119,31 @@ fun ProfileScreen(
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            ProfileHeader(userResult = Result.success(userProfiles[userId]))
-            ProfileStats(userProfile = userProfiles[userId])
-            AboutSection()
-            FriendsSection(
-                friendResult = friendResult,
-                userId = userId,
-                fetchUserById = fetchUserById,
-                userProfilesMap = userProfiles
-            )
+            when(state) {
+                is AuthenticationState.Error,
+                is AuthenticationState.Unauthenticated -> {
+                    Button(
+                        onClick = onLoginClick,
+                        colors = ButtonDefaults.buttonColors(DefaultButton),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp)
+                    ) { Text(stringResource(R.string.settings_section_user_button_login)) }
+
+                }
+                is AuthenticationState.Authenticated -> {
+                    ProfileHeader(userResult = Result.success(userProfiles[userId]))
+                    ProfileStats(userProfile = userProfiles[userId])
+                    AboutSection()
+                    FriendsSection(
+                        friendResult = friendResult,
+                        userId = userId,
+                        fetchUserById = fetchUserById,
+                        userProfilesMap = userProfiles
+                    )
+                }
+                else ->  return@Viewport
+            }
         }
     }
 }
