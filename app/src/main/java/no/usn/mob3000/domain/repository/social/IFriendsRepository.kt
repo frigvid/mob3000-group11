c071de4664b19037ac6765e0f4f14a1d7b6d204a
mr.Hussein Abdul-Ameer <husseinabdulameer11@gmail.com>
2024-11-19 15:41:04 +0100

    Update IFriendsRepository.kt

--- app/src/main/java/no/usn/mob3000/domain/repository/social/IFriendsRepository.kt
+++ app/src/main/java/no/usn/mob3000/domain/repository/social/IFriendsRepository.kt
@@ -7,7 +7,7 @@ import no.usn.mob3000.domain.model.social.FriendData
  *  Interface for fetching friends and non-friends.
  *
  * @author Husseinabdulameer11
- * @created : 2024-11-02
+ * @created  2024-11-02
  */
 interface IFriendsRepository {
     suspend fun fetchFriends(userId: String): Result<List<FriendData>>
