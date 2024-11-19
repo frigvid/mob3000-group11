cf415c0905511307504368ad09fe10930910841a
mr.Hussein Abdul-Ameer <husseinabdulameer11@gmail.com>
2024-11-19 15:39:55 +0100

    Update FriendData.kt

--- app/src/main/java/no/usn/mob3000/domain/model/social/FriendData.kt
+++ app/src/main/java/no/usn/mob3000/domain/model/social/FriendData.kt
@@ -4,11 +4,11 @@ import kotlinx.datetime.Instant
 
 /**
  * @author Husseinabdulameer11
- * created on 05.11.2024
+ * @created on 2024-11-05
  **/
 data class FriendData(
-        val friendshipId: String,
-        val friendsSince: Instant,
-        val user1: String,
-        val user2: String
-    )
+    val friendshipId: String,
+    val friendsSince: Instant,
+    val user1: String,
+    val user2: String
+)
